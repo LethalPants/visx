9c972cb5e0a0d5a7473396650bd9247a8445b9fb
Hatim <hatimmurtuza10@gmail.com>
2020-10-05 12:27:09 +0300

    test: [computeStatus] add test for edge case

--- packages/visx-stats/test/computeStats.test.ts
+++ packages/visx-stats/test/computeStats.test.ts
@@ -1,7 +1,7 @@
 import { computeStats } from '../src';
 
 const data = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 1];
-
+const edgeCaseData = [10000, 2400, 10000, 10000];
 describe('computeStats', () => {
   test('it should be defined', () => {
     expect(computeStats).toBeDefined();
@@ -9,6 +9,18 @@ describe('computeStats', () => {
 
   test('it should have boxPlot and binData', () => {
     const stats = computeStats(data);
+    expect(stats.boxPlot).toBeDefined();
+    expect(stats.binData).toBeDefined();
+  });
+});
+
+describe('computeStatsEdgeCase', () => {
+  test('it should be defined', () => {
+    expect(computeStats).toBeDefined();
+  });
+
+  test('it should have boxPlot and binData', () => {
+    const stats = computeStats(edgeCaseData);
     expect(stats.boxPlot).toBeDefined();
     expect(stats.binData).toBeDefined();
   });
